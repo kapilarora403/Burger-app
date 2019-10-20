22f7ca39c39bbf0628a44b8c43f4599db6e25df3
Kapil Arora <kapilarora403@gmail.com>
2019-10-20 17:30:07 +0530

    Added firebase

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Modal from "../../Components/UI/Modal/Modal";
 import Aux from "../Aux/aux";
 
-const withErrorHandler = (WrappedComponent, axio) => {
+const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
 
         state = {
